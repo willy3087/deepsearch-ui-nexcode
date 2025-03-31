73dceca0311fcdc24387bf7394c130757d86f7b3
willy3087 <will.criando+nexcode-admin@gmail.com>
2025-03-31 12:22:20 -0300

    fix: atualizar a origem base para o servidor
    - Alterada a constante BASE_ORIGIN de "http://localhost:3001" para "http://localhost:3002" para refletir a nova configuração do servidor.

--- app.js
+++ app.js
@@ -229,7 +229,7 @@ const deleteAllSessionsDialog = document.getElementById(
 );
 const navigationDialog = document.getElementById("navigation-dialog");
 
-const BASE_ORIGIN = "http://localhost:3001";
+const BASE_ORIGIN = "http://localhost:3002";
 
 // SVG icons
 const loadingSvg = `<svg id="thinking-animation-icon" width="14" height="14" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_mHwL{animation:spinner_OeFQ .75s cubic-bezier(0.56,.52,.17,.98) infinite; fill:currentColor}.spinner_ote2{animation:spinner_ZEPt .75s cubic-bezier(0.56,.52,.17,.98) infinite;fill:currentColor}@keyframes spinner_OeFQ{0%{cx:4px;r:3px}50%{cx:9px;r:8px}}@keyframes spinner_ZEPt{0%{cx:15px;r:8px}50%{cx:20px;r:3px}}</style><defs><filter id="spinner-gF00"><feGaussianBlur in="SourceGraphic" stdDeviation="1.5" result="y"/><feColorMatrix in="y" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 18 -7" result="z"/><feBlend in="SourceGraphic" in2="z"/></filter></defs><g filter="url(#spinner-gF00)"><circle class="spinner_mHwL" cx="4" cy="12" r="3"/><circle class="spinner_ote2" cx="15" cy="12" r="8"/></g></svg>`;
